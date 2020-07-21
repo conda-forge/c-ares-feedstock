5c08d6bce0cf8d9473652289370d624e9b4e1290
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-21 18:07:23 +0200

    CMAKE_INSTALL_LIBDIR=lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ cmake -G"$CMAKE_GENERATOR" \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
       -DCARES_STATIC=OFF \
       -DCARES_INSTALL=ON \
+      -DCMAKE_INSTALL_LIBDIR=lib \
       ${SRC_DIR}
 
 cmake --build . --config Release --target install
