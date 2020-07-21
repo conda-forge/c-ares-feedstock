eec3224a72cfc255434d2ed26218fc13af750885
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-21 16:59:37 +0200

    Disable static build completly

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,11 @@
 
 mkdir build && cd build
 
-cmake -G "$CMAKE_GENERATOR" \
-      -D CMAKE_BUILD_TYPE=Release \
-      -D CMAKE_INSTALL_PREFIX="$PREFIX" \
-      -D CARES_STATIC=ON \
-      -D CARES_STATIC_PIC=ON \
-      -D CARES_INSTALL=ON \
+cmake -G"$CMAKE_GENERATOR" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+      -DCARES_STATIC=OFF \
+      -DCARES_INSTALL=ON \
       ${SRC_DIR}
 
 cmake --build . --config Release --target install
