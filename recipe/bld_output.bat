b379e7fc6ea48ecd0ed3dc9bee72d263980b8814
Uwe L. Korn <uwelk@xhochy.com>
2020-08-23 07:46:06 +0200

    Build static output separately

---
+++ recipe/bld_output.bat
@@ -0,0 +1,25 @@
+@echo on
+set CMAKE_CONFIG=Release
+
+mkdir build_%CMAKE_CONFIG%
+pushd build_%CMAKE_CONFIG%
+
+if "%PKG_NAME:-6%" eq "static" (
+  set CARES_STATIC=ON
+  set CARES_SHARED=OFF
+) else (
+  set CARES_STATIC=OFF
+  set CARES_SHARED=ON
+)
+
+cmake -GNinja ^
+      -DCARES_STATIC:BOOL=%CARES_STATIC% ^
+      -DCARES_SHARED:BOOL=%CARES_SHARED% ^
+      -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG% ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      "%SRC_DIR%"
+
+ninja install
+if errorlevel 1 exit 1
+
+popd
--- recipe/build_output.sh
+++ recipe/build_output.sh
@@ -2,12 +2,22 @@
 
 mkdir build && cd build
 
+if [[ "$PKG_NAME" == *static ]]; then
+  CARES_STATIC=ON
+  CARES_SHARED=OFF
+else
+  CARES_STATIC=OFF
+  CARES_SHARED=ON
+fi
+
 cmake ${CMAKE_ARGS} -G"$CMAKE_GENERATOR" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCARES_STATIC=OFF \
+      -DCARES_STATIC=${CARES_STATIC} \
+      -DCARES_SHARED=${CARES_SHARED} \
       -DCARES_INSTALL=ON \
       -DCMAKE_INSTALL_LIBDIR=lib \
+      -GNinja \
       ${SRC_DIR}
 
-cmake --build . --config Release --target install
+ninja install
