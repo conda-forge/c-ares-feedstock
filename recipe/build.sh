08278461e6501fd24ff43da8c7d5d119ab3e017e
Uwe L. Korn <uwelk@xhochy.com>
2020-08-23 07:35:55 +0200

    Revert "Added static output (#10)"
    This reverts commit 15ebd6dd30c7e0ae0989ca60e3191204853cdc9f.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,9 @@ mkdir build && cd build
 cmake ${CMAKE_ARGS} -G"$CMAKE_GENERATOR" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCARES_STATIC=ON \
+      -DCARES_STATIC=OFF \
       -DCARES_INSTALL=ON \
       -DCMAKE_INSTALL_LIBDIR=lib \
       ${SRC_DIR}
 
-make
+cmake --build . --config Release --target install
